cf546be462fea9ab17382271ddfde27a9e3f19d6
jpedrolourenco08 <jpedro.lourenco08@gmail.com>
2025-08-25 08:04:52 -0300

    Adição componentes - Menu e CountDown

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,24 @@ import "./styles/global.css";
 import "./styles/theme.css";
 
 import { Container } from "./components/Container/index";
-import { Heading } from "./components/Heading/index";
+//import { Heading } from "./components/Heading/index";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 export function App(){
   return (
     <>
       <Container>
-        <Heading>
           <Logo/>
-        </Heading>
       </Container>
+
        <Container>
-        <Heading>
-          <section>MENU</section>
-        </Heading>
+          <Menu/>
+      </Container>
+
+       <Container>
+          <CountDown/>
       </Container>
     </>
   )
